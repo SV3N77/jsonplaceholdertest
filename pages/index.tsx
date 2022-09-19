6efa184e7f7e05aae9172a67e41a5c50a35ad672
SV3N77 <danny.jiang1234@hotmail.com>
2022-09-19 16:32:09 +1000

    add contact card

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,18 +26,28 @@ function Home({ users }: UserProps) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className="bg-amber-50 p-4">
-        <div className="mx-auto container text-2xl">Jsonplaceholder Users</div>
+      <header className="bg-teal-100 p-4">
+        <div className="mx-auto container text-2xl">
+          Jsonplaceholder Contacts
+        </div>
       </header>
       <main className="py-8">
         <div className="mx-auto container">
           <section className="flex flex-col gap-4">
-            <div className="text-xl ">Users</div>
-            {users.map((user) => (
-              <div key={user.id} className="text-sm">
-                {user.name}
-              </div>
-            ))}
+            <div className="text-xl">Contacts</div>
+            <div className="flex flex-col bg-teal-50">
+              {users.map((user) => (
+                <ContactCards
+                  key={user.id}
+                  name={user.name}
+                  email={user.email}
+                  address={user.address}
+                  phone={user.phone}
+                  website={user.website}
+                  company={user.company}
+                />
+              ))}
+            </div>
           </section>
         </div>
       </main>
@@ -46,3 +56,59 @@ function Home({ users }: UserProps) {
 }
 
 export default Home;
+
+// internal components
+
+type ContactCardProps = {
+  name: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+};
+
+function ContactCards({
+  name,
+  email,
+  address,
+  phone,
+  website,
+  company,
+}: ContactCardProps) {
+  return (
+    <li className="w-full item shadow-sm">
+      <input type="checkbox" id={name} className="peer appearance-none" />
+      <i className="fa-solid fa-chevron-right peer-checked:hidden inline"></i>
+      <i className="fa-solid fa-chevron-down peer-checked:inline hidden"></i>
+      <label htmlFor={name} className="py-4 cursor-pointer grow">
+        <p className="text-base">{name}</p>
+      </label>
+      <div className="content text-sm">
+        <div className="flex gap-2 p-4 justify-between">
+          <div className="flex flex-col gap-2">
+            <div>Email: {email}</div>
+            <div>Phone: {phone}</div>
+            <div>Website: {website}</div>
+          </div>
+          <div className="flex flex-col gap-2">
+            <div>
+              Address:{" "}
+              {address.suite + address.street + address.city + address.zipcode}
+            </div>
+            <div>Company: {company.name}</div>
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+}
